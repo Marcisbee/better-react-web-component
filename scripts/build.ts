e1343eb943565c070f46115813134d91d07c481a
Marcis Bergmanis <marcisbergmanis@gmail.com>
2023-08-09 17:11:24 +0300

    fix for jest

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,20 +1,22 @@
 import * as esbuild from "esbuild";
 
-await esbuild
-	.build({
-		entryPoints: ["./src/react-web-component.ts"],
-		bundle: true,
-		outdir: "dist",
-		outExtension: {
-			".js": ".mjs",
-		},
-		minify: true,
-		sourcemap: "linked",
-		treeShaking: true,
-		target: "es2016",
-		format: "esm",
-		external: ["react", "react-dom", "prop-types"],
-		platform: "browser",
-		logLevel: "info",
-	})
-	.catch(() => process.exit(1));
+for (const format of ["cjs", "esm"] as const) {
+	await esbuild
+		.build({
+			entryPoints: ["./src/react-web-component.ts"],
+			bundle: true,
+			outdir: "dist",
+			outExtension: {
+				".js": format === "esm" ? ".mjs" : ".js",
+			},
+			minify: true,
+			sourcemap: "linked",
+			treeShaking: true,
+			target: "es2016",
+			format,
+			external: ["react", "react-dom", "prop-types"],
+			platform: "browser",
+			logLevel: "info",
+		})
+		.catch(() => process.exit(1));
+}
